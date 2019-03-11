4d86fb8e408ac5ceb4cbb3505a0a8ba61d8b4e18
mcschmitz <manuel.schmitz@gmx.net>
2019-03-11 08:34:58 +0100

    improved plotting

---
+++ chat_bot.py
@@ -0,0 +1,51 @@
+import numpy as np
+from keras.callbacks import EarlyStopping, ModelCheckpoint
+from keras.optimizers import Adam
+
+from CharacterBasedChatBot.network_definition import character_based_lstm
+from DataLoader import CharacterEmbedder
+from plot_loss import PlotLearning
+from preprocess_data import DATA_OUT_PATH as DATA_INPUT_PATH
+
+# PARAMETERS
+MIN_SEQ_LEN = 1
+MAX_SEQ_LEN = 120
+BATCH_SIZE = 128
+EPOCHS = 1000
+LATENT_DIM = 256
+MODEL_PATH = 'models/CharacterBasedChatBot.h5'
+PROGRESS_PATH = "models/CharacterBasedChatBot_progress.png"
+
+loader = CharacterEmbedder(DATA_INPUT_PATH)
+loader.filter_sequences(min_sequence_length=MIN_SEQ_LEN, max_sequence_length=MAX_SEQ_LEN)
+loader.create_char_dictionary(loader.short_questions, loader.short_answers)
+escape_dict = {"eos": "$", 'unk': "§"}
+loader.char_dictionary_to_int(answers=loader.short_answers, questions=loader.short_questions, escape_codes=escape_dict)
+loader.txt2int(loader.short_questions, loader.short_answers)
+
+questions = loader.short_questions
+answers = loader.short_answers
+answers_int = loader.answers_int
+answers_int_to_vocab = loader.answers_int_to_wd
+questions_int = loader.questions_int
+questions_int_to_vocab = loader.questions_int_to_wd
+
+encoder_input_data = np.zeros((len(questions), MAX_SEQ_LEN + 1, len(questions_int_to_vocab)), dtype='float32')
+decoder_target_data = np.zeros((len(answers), MAX_SEQ_LEN + 1, len(answers_int_to_vocab)), dtype='float32')
+
+for idx, question_int in enumerate(questions_int):
+    for t, q in enumerate(question_int):
+        encoder_input_data[idx, t, q] = 1
+
+for idx, answer_int in enumerate(answers_int):
+    for t, q in enumerate(answer_int):
+        decoder_target_data[idx, t, q] = 1
+
+early_stopper = EarlyStopping(patience=100)
+model_saver = ModelCheckpoint(MODEL_PATH, save_best_only=True)
+progress_plotter = PlotLearning(path=PROGRESS_PATH)
+model = character_based_lstm(timesteps=MAX_SEQ_LEN + 1, num_encoder_tokens=len(questions_int_to_vocab),
+                             num_decoder_tokens=len(answers_int_to_vocab))
+model.compile(optimizer=Adam(.0005), loss='categorical_crossentropy', metrics=["accuracy"])
+model.fit(encoder_input_data, decoder_target_data, batch_size=BATCH_SIZE, epochs=EPOCHS,
+          validation_split=0.25, callbacks=[early_stopper, model_saver, progress_plotter], verbose=1)
--- plot_loss.py
+++ plot_loss.py
@@ -4,33 +4,44 @@ from matplotlib import pyplot as plt
 
 
 class PlotLearning(Callback):
-    def __init__(self, file_path):
-        super(PlotLearning).__init__()
-        self.file_path = file_path
+    def __init__(self, path):
+        super(PlotLearning, self).__init__()
+        if path:
+            plt.ioff()
+            self.path = path
+        self.i = 0
+        self.x = []
+        self.metrics = {}
+        self.val_metrics = {}
+        self.fig = None
+        self.logs = []
 
     def on_train_begin(self, logs={}):
         self.i = 0
         self.x = []
-        self.losses = []
-        self.val_losses = []
-        self.acc = []
-        self.val_acc = []
+        self.metrics = {m: [] for m in self.model.metrics_names}
+        self.val_metrics = {m: [] for m in self.model.metrics_names}
         self.fig = plt.figure()
         self.logs = []
 
     def on_epoch_end(self, epoch, logs={}):
         self.logs.append(logs)
         self.x.append(self.i)
-        self.losses.append(logs.get('loss'))
-        self.val_losses.append(logs.get('val_loss'))
+        for metric in self.model.metrics_names:
+            self.metrics[metric].append(logs.get(metric))
+            self.val_metrics[metric].append(logs.get('val_' + metric))
         self.i += 1
-        f, (ax1) = plt.subplots(1, sharex=True)
+
+        f, ax = plt.subplots(1, len(self.model.metrics_names), sharex=True, figsize=(16, 6))
 
         clear_output(wait=True)
 
-        ax1.set_yscale('log')
-        ax1.plot(self.x, self.losses, label="loss")
-        ax1.plot(self.x, self.val_losses, label="val_loss")
-        ax1.legend()
-
-        plt.save(self.file_path)
+        for idx, metric in enumerate(self.model.metrics_names):
+            ax[idx].plot(self.x, self.metrics[metric], label=metric)
+            ax[idx].plot(self.x, self.val_metrics[metric], label="validation " + metric)
+            ax[idx].legend()
+        if self.path:
+            plt.savefig(self.path)
+        else:
+            plt.show()
+        plt.close()
